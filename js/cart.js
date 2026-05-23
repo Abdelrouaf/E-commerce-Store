a5da91809b2f2afa700f426ef72ef488e619dd87
Abdelraouf <bodi.halaby654@gmail.com>
2026-05-23 20:17:56 +0300

    Fix Bugs

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,36 @@
 
 let cartContainer2 = JSON.parse(localStorage.getItem("addToCart")) || [];
 
+function displayCart() {
+    let tbody2 = document.getElementById("tBody2");
+    let boom2 = '';
+    for (let i = 0; i < cartContainer2.length; i++) {
+        boom2 +=
+        `
+        
+        <tr>
+        
+            <td class="image"><a href="#"><img src="${cartContainer2[i].imageSrc}" alt=""></a></td>
+        
+            <td class="product-name"><a href="#">${cartContainer2[i].title}</a></td>
+        
+            <td class="quantity"><p><button class="minus counter" onclick="minusNumber(${i})"><i class="fa-solid fa-minus"></i></button> <span class="number">${cartContainer2[i].count}</span> <button class="plus counter" onclick="plusNumber(${i})"><i class="fa-solid fa-plus"></i></button></p></td>
+        
+            <td class="product-price"><span class="dollar">$</span><span class="subtotal">${cartContainer2[i].price}.00</span></td>
+        
+            <td class="delete"><button class="btn btn-primary homeBtn" onclick="deleteRow(${i})"><i class="fa-regular fa-trash-can"></i></button></td>
+    
+        </tr>
+        
+        `
+    }
+    tbody2.innerHTML = boom2;
+    sumTotal();
+}
+
 let cartTotals = document.getElementById("cartTotals");
 let totalPrice = document.getElementById("totalPrice");
 let totalPrice2 = document.getElementById("totalPrice2");
-let total = 0;
 
 let statusEmpty = document.getElementById("statusEmpty");
 let statusFull = document.getElementById("statusFull");
@@ -16,11 +42,14 @@ if (cartContainer2.length === 0) {
 } else {
     statusFull.classList.remove("dnone");
     statusEmpty.classList.add("dnone");
+    displayCart();
     sumTotal();
 }
 
+let total = 0;
 function sumTotal() {
-    for (let i = 0 ; i < cartContainer2.length; i++) {
+    total = 0; // ← ADD THIS RESET LINE
+    for (let i = 0; i < cartContainer2.length; i++) {
         total += cartContainer2[i].price;
     }
     totalPrice.textContent = total;
@@ -50,33 +79,6 @@ removeCart.addEventListener("click", function () {
     location.reload();
 })
 
-function displayCart() {
-    let tbody2 = document.getElementById("tBody2");
-    let boom2 = '';
-    for (let i = 0; i < cartContainer2.length; i++) {
-        boom2 +=
-        `
-        
-        <tr>
-        
-            <td class="image"><a href="#"><img src="${cartContainer2[i].imageSrc}" alt=""></a></td>
-        
-            <td class="product-name"><a href="#">${cartContainer2[i].title}</a></td>
-        
-            <td class="quantity"><p><button class="minus counter" onclick="minusNumber(${i})"><i class="fa-solid fa-minus"></i></button> <span class="number">${cartContainer2[i].count}</span> <button class="plus counter" onclick="plusNumber(${i})"><i class="fa-solid fa-plus"></i></button></p></td>
-        
-            <td class="product-price"><span class="dollar">$</span><span class="subtotal">${cartContainer2[i].price}.00</span></td>
-        
-            <td class="delete"><button class="btn btn-primary homeBtn" onclick="deleteRow(${i})"><i class="fa-regular fa-trash-can"></i></button></td>
-    
-        </tr>
-        
-        `
-    }
-    tbody2.innerHTML = boom2;
-    sumTotal();
-}
-
 let num = document.querySelector(".number");
 let minus = document.querySelector(".minus");
 let plus = document.querySelector(".plus");
@@ -99,9 +101,9 @@ function minusNumber(i) {
         localStorage.setItem("addToCart", JSON.stringify(cartContainer2));
     } else {
         cartContainer2[i].count -= 1;
-        for (let j = 0; j < shop.length; j++) {
-            if(shop[j].id == cartContainer2[i].id) {
-                cartContainer2[i].price -= shop[j].price;
+        for (let j = 0; j < shop2.length; j++) {
+            if(shop2[j].id == cartContainer2[i].id) {
+                cartContainer2[i].price -= shop2[j].price;
                 break;
             }
         }
@@ -113,9 +115,9 @@ function minusNumber(i) {
 
 function plusNumber(i) {
     cartContainer2[i].count += 1;
-    for (let j = 0; j < shop.length; j++) {
-        if(shop[j].id == cartContainer2[i].id) {
-            cartContainer2[i].price += shop[j].price;
+    for (let j = 0; j < shop2.length; j++) {
+        if(shop2[j].id == cartContainer2[i].id) {
+            cartContainer2[i].price += shop2[j].price;
             break;
         }
     }
